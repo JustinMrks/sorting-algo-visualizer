23d4bc513b8cca2d256b63b8ff66666ed7cfde95
justinmrks <justinmarks19@gmail.com>
2023-07-05 19:18:23 -0500

    responsively makes a new array and fits to screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,41 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return <div className="App"></div>;
+  const arraySize = Math.floor(window.innerWidth / 16);
+  const [array, setArray] = useState([]);
+
+  const constructArray = () => {
+    setArray(
+      Array.from(
+        { length: arraySize },
+        () => Math.floor(Math.random() * 100) + 1
+      )
+    );
+  };
+
+  const sortArray = () => {
+    let newthing = array.sort((a, b) => a - b);
+    setArray([...newthing]);
+  };
+
+  useEffect(() => constructArray(), [arraySize]);
+
+  return (
+    <div className="App">
+      <button onClick={constructArray}>click</button>
+      <button onClick={sortArray}>sort</button>
+      <div className="array-container">
+        {array.map((num, idx) => (
+          <div
+            className="array-bar"
+            key={idx}
+            style={{ height: `${num * ((window.innerHeight - 50) / 100)}px` }}
+          ></div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
